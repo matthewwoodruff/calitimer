70b953bd74440831e889e0d4126869250759e6fa
Matthew Woodruff <mwoodruf@thoughtworks.com>
2020-08-18 14:17:56 +0100

    Add built files to test github file serving

---
+++ tmp/precache-manifest.7b309fe0b525866eef1412ef3ae33388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8a545d8a9d6a8e9326eb697245e135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3249b1890ee6e6d83f58",
+    "url": "/static/css/main.6b5ee91c.chunk.css"
+  },
+  {
+    "revision": "d9de9c3f24b84c38ed58",
+    "url": "/static/js/2.b960f245.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.b960f245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3249b1890ee6e6d83f58",
+    "url": "/static/js/main.faaeab37.chunk.js"
+  },
+  {
+    "revision": "f93febdc7525e96e1806",
+    "url": "/static/js/runtime-main.b18e75d3.js"
+  }
+]);
